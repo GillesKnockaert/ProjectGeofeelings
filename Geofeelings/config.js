d04ec84c8650e02f11eda2bee02634b6f6b4464e
Fgryspee <frederic.gryspeerdt@student.howest.be>
2015-12-29 15:40:45 +0100

    Update config file

--- Geofeelings/config.js
+++ Geofeelings/config.js
@@ -4,12 +4,14 @@ var config = {
     HOST: 'http://localhost',
     PORT: getEnv('PORT') || 3000,
     MONGODBURL : setMongoDbUrl(getEnv('NODE_ENV')),
-    SECRET : 'geofeelingssecret'  //om JSON Web Tokens te maken en verifiëren
+    SECRET : 'geofeelingssecret'  //om JSON Web Tokens te maken en verifiï¿½ren
 };
 
 function getEnv(variable) {
     if (process.env[variable] === undefined) {
         if (variable == 'PORT') { return 1337 };
+        if (variable == 'NODE_ENV') { return 'production' };
+
         console.log('You must create an environment variable for ' + variable);
     }
     return process.env[variable]; //of bvb. process.env.WEB_PORT
@@ -18,7 +20,7 @@ function getEnv(variable) {
 
 function setMongoDbUrl(nodeEnv){
     if (nodeEnv == 'production'){
-        return  process.env.MONGO_URI;
+        return  process.env.MONGO_URI || "mongodb://fgryspee:geofeelings@ds034198.mongolab.com:34198/geofeelings";
     } else {
         return process.env.MONGO_LOCAL;
     }
