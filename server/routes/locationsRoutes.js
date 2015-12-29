eabb5d8bb6febc993f68f985088de2f04efec1b9
Fgryspee <frederic.gryspeerdt@student.howest.be>
2015-12-29 18:32:21 +0100

    Fix Heroku error

--- server/routes/locationsRoutes.js
+++ server/routes/locationsRoutes.js
@@ -2,7 +2,7 @@
  * Created by Frederic on 26/12/2015.
  */
 
-var express = require('Express');
+var express = require('express');
 
 var routes = function(Location){
     var locationRouter = express.Router();
